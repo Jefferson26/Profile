19d681ddc0882becba36882730b52f948462aaec
Jefferson Barbosa <jefferson.sb.es@gmail.com>
2019-06-20 18:22:32 -0300

    Created Avatar layout and description text layout. Added new fonts

--- src/components/AboutMe/aboutme.js
+++ src/components/AboutMe/aboutme.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-import TimeLine from "./timeline";
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
-import Profile from "../../resources/images/avatar1.jpg";
+import Profile from "./profile";
 
 class About extends Component{
     render(){
         return (
             <div>
-                <Grid container justify="center" alignItems="center">
-                    <Avatar alt="Jeff B." src={Profile} />
-                </Grid>
-                <TimeLine/>
+                <Profile/>
             </div>
         )
     }
