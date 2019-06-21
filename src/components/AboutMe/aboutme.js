5342074e0c48dfbf1bdd93afb79738bc6364efc4
Jefferson Barbosa <jefferson.sb.es@gmail.com>
2019-06-21 11:15:36 -0300

    Defined layout paddings and skills layout

--- src/components/AboutMe/aboutme.js
+++ src/components/AboutMe/aboutme.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Profile from "./profile";
+import Skills from "./skills";
+import Styles from "./aboutme.css";
 
 class About extends Component{
     render(){
         return (
-            <div>
+            <div className={Styles.mainDiv}>
                 <Profile/>
+                <Skills/>
             </div>
         )
     }
