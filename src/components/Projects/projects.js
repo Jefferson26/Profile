9cc1ca705bd1f90e6a533b340aa4ce99d916c90e
jefferson-osf <jefferson.barbosa@ifactory.com.br>
2019-06-26 17:06:29 -0300

    Added timeline to show all the projects that I already done

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Styles from "./projects.css";
 import Tittle from "../tittle";
+import TimeLine from "./timeline";
 
 class Projects extends Component{
     render(){
         return (
             <div className={Styles.mainDivProjects}>
                 <Tittle tittleName={"Projects"}/>
+                <TimeLine/>
             </div>
         )
     }
